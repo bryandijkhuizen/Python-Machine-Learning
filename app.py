0404d31d0fbbcc6f176e63beae19836030e240f0
Bryan Dijkhuizen <45204595+bryandijkhuizen@users.noreply.github.com>
2019-05-16 19:47:10 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+import quandl as q
+import pandas as pd
+
+#df = dataframe
+
+df = q.get('WIKI/GOOGL')
+df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume',]]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+
+forecast_col = 'Adj. Close'
+df.fillna(-99999, inplace=True)
+
+#fills the empty data
+
+
+
+
